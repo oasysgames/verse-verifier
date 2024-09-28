680d677a74bd90c99be03678dec7d292b26f0c29
ironbeer <7997273+ironbeer@users.noreply.github.com>
2024-09-28 19:21:57 +0900

    Add generics SyncMap

--- util/sync_test.go
+++ util/sync_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -33,4 +34,66 @@ func TestReleaseGuardSemaphore(t *testing.T) {
 	gsem.ReleaseALL()
 	gsem.Acquire(parent, 2)
 	shouldErr()
+}
+
+func TestSyncMap(t *testing.T) {
+	var boolmap SyncMap[string, bool]
+	got0, ok := boolmap.Load("key")
+	assert.False(t, got0)
+	assert.False(t, ok)
+
+	boolmap.Store("key", true)
+	got1, ok := boolmap.Load("key")
+	assert.True(t, got1)
+	assert.True(t, ok)
+	boolmap.Range(func(key string, value bool) bool {
+		assert.Equal(t, "key", key)
+		assert.True(t, value)
+		return true
+	})
+
+	boolmap.Delete("key")
+	got2, ok := boolmap.Load("key")
+	assert.False(t, got2)
+	assert.False(t, ok)
+
+	type item struct{ id int }
+	var structmap SyncMap[string, *item]
+	got3, ok := structmap.Load("key")
+	assert.Nil(t, got3)
+	assert.False(t, ok)
+
+	structmap.Store("key1", &item{id: 100})
+	got4, ok := structmap.Load("key1")
+	assert.Equal(t, 100, got4.id)
+	assert.True(t, ok)
+
+	got5, loaded := structmap.Swap("key1", &item{id: 101})
+	got6, ok := structmap.Load("key1")
+	assert.Equal(t, 100, got5.id)
+	assert.Equal(t, 101, got6.id)
+	assert.True(t, loaded)
+	assert.True(t, ok)
+
+	got7, loaded := structmap.Swap("key2", &item{id: 102})
+	assert.Nil(t, got7)
+	assert.False(t, loaded)
+
+	got8, ok := structmap.Load("key2")
+	assert.Equal(t, 102, got8.id)
+	assert.True(t, ok)
+
+	total := 0
+	structmap.Range(func(key string, value *item) bool {
+		total++
+		assert.Contains(t, []string{"key1", "key2"}, key)
+		assert.Contains(t, []int{101, 102}, value.id)
+		return true
+	})
+	assert.Equal(t, 2, total)
+
+	structmap.Delete("key1")
+	got9, ok := structmap.Load("key1")
+	assert.Nil(t, got9)
+	assert.False(t, ok)
 }
